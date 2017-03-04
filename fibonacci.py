3b20c6a121c4e663b0e259e756befe131036e35c
skuarch <skuarch@yahoo.com.mx>
2017-03-04 10:23:47 -0600

    firts commit

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 11:55:34 2016
+
+@author: skuarch
+"""
+
+def finonacci(num):
+    start = 0
+    end = num
+    while(start < end):
+        r = start + start + 1
+        print r
